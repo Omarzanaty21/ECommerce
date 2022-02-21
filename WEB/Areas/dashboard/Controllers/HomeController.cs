598420335bd17e2e1decce433d320d23e4dcb029
Abdelrahman Osama <eslamboully@gmail.com>
2022-02-21 14:06:09 +0200

    fix dashboard design

--- WEB/Areas/dashboard/Controllers/HomeController.cs
+++ WEB/Areas/dashboard/Controllers/HomeController.cs
@@ -2,8 +2,10 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 namespace WEB.Areas.dashboard.Controllers
 {
+    [Route("/dashboard")]
     public class HomeController : dashboardBaseController
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
